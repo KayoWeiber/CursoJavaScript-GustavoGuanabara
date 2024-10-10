89e3e586fa3f4fc1a0f244b6391bf6cfbca4f5b3
Kayo Weiber <caioveiber598@gmail.com>
2024-10-10 17:17:47 -0300

    aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,4 @@
+var vel = 50
+console.log (`a velocidade do seu carro é ${vel}`) 
+if(vel>60){console.log('você foi multado!')}
+console.log('dirija com cuidado')
---
+++ aula11/ex009.js
@@ -0,0 +1,3 @@
+var estado='SP'
+console.log (`você mora no estado de ${estado}`) 
+if (estado=='Minas') {console.log('VOCÊ É MINEIRO')} else {console.log ('você é Paulista')}
