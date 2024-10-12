b5dfdafaefb13b3b3dce42b6fbc871595b98e1bd
Kayo Weiber <caioveiber598@gmail.com>
2024-10-12 08:04:04 -0300

    aula12

---
+++ aula12/ex013.js
@@ -0,0 +1,18 @@
+var day=new Date()
+var semday=day.getDay()
+console.log(semday)
+switch(semday){
+    case 0: console.log('domingo')
+    break
+    case 1:console.log('segunda')
+    break
+    case 2: console.log('terça')
+    break
+    case 3: console.log('quarta')
+    break
+    case 4: console.log('quinta')
+    break
+    case 5: console.log('sexta')
+    break
+    case 6: console.log('sabado')
+}
